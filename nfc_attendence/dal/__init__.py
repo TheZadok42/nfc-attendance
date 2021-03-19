eb0816f4348a2edca1509c55b7aeb92e756c5232
Tal <zadok.tal1@gmail.com>
2021-03-19 17:14:02 +0200

    order

--- nfc_attendence/dal/__init__.py
+++ nfc_attendence/dal/__init__.py
@@ -7,8 +7,22 @@ from .base import Base
 from .check_point import CheckPoint
 from .group import Group
 
-conn_string = 'sqlite:///A:\\Dev\\nfc-attendance\\test.sqlite3'
-engine = sqlalchemy.create_engine(conn_string)
-Base.metadata.create_all(engine)
 
-SessionBase = sessionmaker(engine)
+def get_engine(uri=None):
+    return sqlalchemy.create_engine(f"sqlite:///{uri or ''}", connect_args={"check_same_thread": False})
+
+
+def get_session_base(engine):
+    session_base = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    try:
+        yield session_base
+    finally:
+        pass
+
+
+def get_session(session_base):
+    session = session_base()
+    try:
+        yield session
+    finally:
+        session.close()
--- nfc_attendence/server/main.py
+++ nfc_attendence/server/main.py
@@ -3,12 +3,16 @@ from fastapi import FastAPI
 from models.group import Group, CreateGroup
 from nfc_attendence import dal
 
+db_engine = dal.get_engine()
+session_base = dal.get_session_base(db_engine)
+dal.Base.metadata.create_all(db_engine)
+
 app = FastAPI()
 
 
 @app.post("/groups", response_model=Group)
 async def create_group(group: CreateGroup):
-    session = dal.SessionBase()
+    session = dal.get_session(session_base)
     db_group = dal.Group(name=group.name, description=group.description)
     session.add(db_group)
     session.commit()
