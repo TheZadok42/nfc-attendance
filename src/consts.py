3be841422a08cc898eeaa7cbe31476639edf17ae
Tal <zadok.tal1@gmail.com>
2021-06-26 12:55:17 +0300

    new font

--- src/consts.py
+++ src/consts.py
@@ -2,4 +2,4 @@ import pathlib
 
 ROOT = pathlib.Path(__file__).absolute().parent
 FONTS_DIR = ROOT.joinpath("../assets/fonts")
-TEXT_FONT_PATH = FONTS_DIR.joinpath("monofonto.ttf")
+TEXT_FONT_PATH = FONTS_DIR.joinpath("Minecraftia.ttf")
