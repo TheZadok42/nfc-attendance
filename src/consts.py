a550fd21408b5ed9f6cc9b00eca950ae698baf08
Tal <zadok.tal1@gmail.com>
2021-06-26 12:37:31 +0300

    Fixed path

--- src/consts.py
+++ src/consts.py
@@ -1,5 +1,5 @@
 import pathlib
 
 ROOT = pathlib.Path(__file__).absolute().parent
-FONTS_DIR = ROOT.joinpath("./assets/fonts")
+FONTS_DIR = ROOT.joinpath("../assets/fonts")
 TEXT_FONT_PATH = FONTS_DIR.joinpath("FreePixel.ttf")
