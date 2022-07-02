5ae4b2f07f499a970d4a1508d6dae0cbbfbbe994
TheZadok42 <zadok.tal1@gmail.com>
2022-07-02 10:45:55 +0300

    attendace to attendance

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ import httpx
 
 from .wrappers import NFCWrapper, ScreenWrapper
 
-# TODO: remove prints
-DAL_BASE_URL = 'http://192.168.1.105:8001'
+DAL_BASE_URL = 'https://nfc-attendance-achva.herokuapp.com/'
 
 
 def wait_for_card(nfc: NFCWrapper):
@@ -15,9 +14,10 @@ def wait_for_card(nfc: NFCWrapper):
     return uid
 
 
-async def send_user_atendance(uid: str):
+async def send_user_attendance(uid: str):
     async with httpx.AsyncClient(base_url=DAL_BASE_URL) as client:
-        response = await client.post('/attendance', params=dict(uid=uid))
+        response = await client.post('/attendance/',
+                                     params=dict(nfc_card_uid=uid))
         response.raise_for_status()
 
 
@@ -27,10 +27,13 @@ async def user_wait_loop(nfc: NFCWrapper, screen: ScreenWrapper):
         uid = wait_for_card(nfc)
         screen.write('Registering')
         try:
-            await send_user_atendance(uid)
+            await send_user_attendance(uid)
         except httpx.HTTPStatusError as error:
             screen.write('Failed to Register Attendance')
-            print(error.response.json())
+            try:
+                print(error.response.json())
+            except:
+                print(error.response.content)
         else:
             screen.write('Successfully Registered Attendance')
         time.sleep(1)
